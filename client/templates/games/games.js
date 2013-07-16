dc38a3c6359873d3f93eec65da7e8386a9e5fce2
Andrew Becker <abecks@gmail.com>
2013-07-16 12:41:46 -0700

    Added admin games list

--- client/templates/games/games.js
+++ client/templates/games/games.js
@@ -1,6 +1,13 @@
 // Create minimongo objects
 Games = new Meteor.Collection("games");
 
+// Subscribe to games if we're on that action
+Deps.autorun(function(){
+    if(Session.get('action') == 'games'){
+        var gamesHandler = Meteor.subscribe('games');
+    }
+});
+
 Template.games.games = function () {
     return Games.find({}, {sort: {created: 'asc'}});
 };
